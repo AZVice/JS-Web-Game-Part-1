e46a40f0b1ff6605e2c94c86b8cb45c1bbc65754
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-09-22 13:33:13 -0700

    Completed part iii

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+//Background code starts here. 
 function tile(src, positionLeft, positionBottom, width, height){
     for(let h = 0; h < height; h++){
         for(let w = 0; w < width; w++){
@@ -6,6 +6,17 @@ function tile(src, positionLeft, positionBottom, width, height){
         }
     }
 }
+
+let horizon = window.innerHeight / 1.75;
+let heightOfSky = window.innerHeight - horizon;
+let heightOfGrass = horizon;
+
+tile("assets/sky.png", 0, horizon, window.innerWidth/100, heightOfSky/100);
+tile("assets/grass.png", 0, 0, window.innerWidth/100, heightOfGrass/100);
+
+//Background code ends here. 
+
+//Adding imgaes to the screen code starts here. 
 
 function newImage (src, positionLeft, positionBottom){  // When this fuction is executed all images appear on screen. Added as arguments below. 
 
@@ -19,19 +30,35 @@ function newImage (src, positionLeft, positionBottom){  // When this fuction is 
     return landImages;
 }
 
+newInventory()
+newImage("assets/green-character.gif", 100, 100);
+newImage("assets/pine-tree.png", 450, 200);
+newImage("assets/tree.png", 200, 300 );
+newImage("assets/pillar.png",350, 100);
+newImage("assets/crate.png", 250, 200);
+newImage("assets/well.png", 500, 425);
+newImage("assets/red-character.gif", 50, 150); //testing adding characters. 
+
+//Adding images to screen ends here... 
+
+//Adding inventory items a starts here.
+
+const inventory = newInventory()
 
 function newItem(src, positionLeft, positionBottom){ // This fuction runs to add new items on screen. 
     let  landImages = newImage(src, positionLeft, positionBottom); //links newImage and newItem functions. Not sure about this statememt. 
 
         landImages.addEventListener("dblclick", () => {// Items disappeared after double clicking the on the item. 
             landImages.remove();
-            let.remove()
             let inventoryItem = document.createElement("img")
-            inventoryItem.url = src
+            inventoryItem.src = src
             inventory.append(inventoryItem)
         })
+        return landImages
+
 }
-let inventory
+
+
 function newInventory() {
     let inventory = document.createElement('div')
     inventory.style.position = 'fixed'
@@ -46,29 +73,14 @@ function newInventory() {
     inventory.style.border = '2px solid black'
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
+    return inventory
+    
 }
 
-let horizon = window.innerHeight / 1.75;
-let heightOfSky = window.innerHeight - horizon;
-let heightOfGrass = horizon;
-
-tile("assets/sky.png", 0, horizon, window.innerWidth/100, heightOfSky/100);
-tile("assets/grass.png", 0, 0, window.innerWidth/100, heightOfGrass/100);
-
-
-newInventory()
-newImage("assets/green-character.gif", 100, 100);
-newImage("assets/pine-tree.png", 450, 200);
-newImage("assets/tree.png", 200, 300 );
-newImage("assets/pillar.png",350, 100);
-newImage("assets/crate.png", 250, 200);
-newImage("assets/well.png", 500, 425);
-newImage("assets/red-character.gif", 50, 150); //testing adding characters. 
 
 
 newItem("assets/sword.png",500,405);
 newItem("assets/shield.png", 165, 185);
 newItem("assets/staff.png", 600, 250);
-
 
 
